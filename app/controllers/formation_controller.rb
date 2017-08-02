8b0030c74f95b575d30320ccf92f1baa50d68df7
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-02 16:06:42 -0700

    Set up routes in the formation controller

--- app/controllers/formation_controller.rb
+++ app/controllers/formation_controller.rb
@@ -1,2 +1,22 @@
 class FormationController < ApplicationController
+
+  get '/formation/new' do
+    erb :'formation/new'
+  end
+
+  get "/formation/:id" do
+    @formation = Formation.find(params[:id])
+    erb :'formation/show'
+  end
+
+  get "/formation/:id/edit" do
+    @formation = Formation.find(params[:id])
+    erb :'formation/edit'
+  end
+
+  patch "/formation/:id" do
+    @formation = Formation.find(params[:id])
+    redirect "/formation/#{@formation.id}"
+  end
 end
+
