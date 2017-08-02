190cfcc923d63334d45be731d8107629422e36f7
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-02 14:02:17 -0700

    Set up Application Controller including helper methods for current user

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+require './config/environment'
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  helpers do
+    def current_user
+      Team.find(session[:user_id])
+    end
+
+    def is_logged_in?
+      !!session[:user_id]
+    end
+  end
+end
