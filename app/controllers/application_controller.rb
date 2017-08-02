1c43a6050e5d790c9d5f792e4012ebb5127b70ae
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-02 15:45:01 -0700

    Set up login and signin controller actions and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,45 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    @team = Team.new(:name => params[name], :password => params[password])
+    if @team.save && !params[name].empty?
+      redirect "/teams/#{@team.id}"
+    else
+      redirect '/failure'
+    end
+  end
+
+  get '/failure' do
+    erb :failure
+  end
+
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+  get 'teams/:id' do
+    @team = Team.find(params[:id])
+    erb :'team/show'
+  end
+
   helpers do
     def current_user
       Team.find(session[:user_id])
