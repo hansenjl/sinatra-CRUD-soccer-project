13074741aefe85a03a2798716742011887d45802
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-04 10:34:58 -0700

    Change helper methods to tell the formations that the positions belong to it

--- app/controllers/formation_controller.rb
+++ app/controllers/formation_controller.rb
@@ -14,19 +14,17 @@ class FormationController < ApplicationController
   post '/formation' do
     case params[:formation]
     when "4-4-2"
-      @formation = Formation.new
-      default_4_4_2(@formation.id)
+      @formation = Formation.new(:name => "4-4-2")
+      default_4_4_2(@formation)
     when "4-3-3"
-      @formation = Formation.new
-      default_4_3_3(@formation.id)
+      @formation = Formation.new(:name => "4-3-3")
+      default_4_3_3(@formation)
     else
       redirect '/formation/new'
     end
     @formation.teams << current_team
     @formation.save
-    binding.pry
     redirect "/formation/#{@formation.id}/edit"
-
   end
 
   get "/formation/:id" do
@@ -57,33 +55,33 @@ class FormationController < ApplicationController
   end
 
   helpers do
-    def default_4_4_2(formation_id)
-      Position.create(:name => "left forward",:formation_id => formation_id)
-      Position.create(:name => "right forward",:formation_id => formation_id)
-      Position.create(:name => "center mid 1",:formation_id => formation_id)
-      Position.create(:name => "left mid",:formation_id => formation_id)
-      Position.create(:name => "center mid 2",:formation_id => formation_id)
-      Position.create(:name => "right mid",:formation_id => formation_id)
-      Position.create(:name => "left defender",:formation_id => formation_id)
-      Position.create(:name => "stopper",:formation_id => formation_id)
-      Position.create(:name => "right defender",:formation_id => formation_id)
-      Position.create(:name => "sweeper",:formation_id => formation_id)
-      Position.create(:name => "goalie",:formation_id => formation_id)
+    def default_4_4_2(formation)
+      formation.positions << Position.create(:name => "left forward",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "right forward",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "center mid 1",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "left mid",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "center mid 2",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "right mid",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "left defender",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "stopper",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "right defender",:formation_id => formation.id)
+     formation.positions <<  Position.create(:name => "sweeper",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "goalie",:formation_id => formation.id)
     end
 
-    def default_4_3_3(formation_id)
+    def default_4_3_3(formation)
 
-      Position.create(:name => "left forward",:formation_id => formation_id)
-      Position.create(:name => "center forward",:formation_id => formation_id)
-      Position.create(:name => "right forward",:formation_id => formation_id)
-      Position.create(:name => "left mid",:formation_id => formation_id)
-      Position.create(:name => "center mid",:formation_id => formation_id)
-      Position.create(:name => "right mid",:formation_id => formation_id)
-      Position.create(:name => "left defender",:formation_id => formation_id)
-      Position.create(:name => "stopper",:formation_id => formation_id)
-      Position.create(:name => "right defender",:formation_id => formation_id)
-      Position.create(:name => "sweeper",:formation_id => formation_id)
-      Position.create(:name => "goalie",:formation_id => formation_id)
+      formation.positions << Position.create(:name => "left forward",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "center forward",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "right forward",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "left mid",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "center mid",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "right mid",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "left defender",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "stopper",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "right defender",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "sweeper",:formation_id => formation.id)
+      formation.positions << Position.create(:name => "goalie",:formation_id => formation.id)
     end
   end
 end
