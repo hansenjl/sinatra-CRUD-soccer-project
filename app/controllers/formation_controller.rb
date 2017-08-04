beaa9b6b8ed0739adee46f7b104aa83a6f827ba3
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-04 10:26:52 -0700

    Restructure the create formation functionality so that seeding data is not needed

--- app/controllers/formation_controller.rb
+++ app/controllers/formation_controller.rb
@@ -12,9 +12,21 @@ class FormationController < ApplicationController
 
 
   post '/formation' do
-    @formation = Formation.find_by(:name => params[:formation_name])
+    case params[:formation]
+    when "4-4-2"
+      @formation = Formation.new
+      default_4_4_2(@formation.id)
+    when "4-3-3"
+      @formation = Formation.new
+      default_4_3_3(@formation.id)
+    else
+      redirect '/formation/new'
+    end
     @formation.teams << current_team
+    @formation.save
+    binding.pry
     redirect "/formation/#{@formation.id}/edit"
+
   end
 
   get "/formation/:id" do
@@ -41,6 +53,37 @@ class FormationController < ApplicationController
       redirect "/formation/#{@formation.id}"
     else
       redirect '/'
+    end
+  end
+
+  helpers do
+    def default_4_4_2(formation_id)
+      Position.create(:name => "left forward",:formation_id => formation_id)
+      Position.create(:name => "right forward",:formation_id => formation_id)
+      Position.create(:name => "center mid 1",:formation_id => formation_id)
+      Position.create(:name => "left mid",:formation_id => formation_id)
+      Position.create(:name => "center mid 2",:formation_id => formation_id)
+      Position.create(:name => "right mid",:formation_id => formation_id)
+      Position.create(:name => "left defender",:formation_id => formation_id)
+      Position.create(:name => "stopper",:formation_id => formation_id)
+      Position.create(:name => "right defender",:formation_id => formation_id)
+      Position.create(:name => "sweeper",:formation_id => formation_id)
+      Position.create(:name => "goalie",:formation_id => formation_id)
+    end
+
+    def default_4_3_3(formation_id)
+
+      Position.create(:name => "left forward",:formation_id => formation_id)
+      Position.create(:name => "center forward",:formation_id => formation_id)
+      Position.create(:name => "right forward",:formation_id => formation_id)
+      Position.create(:name => "left mid",:formation_id => formation_id)
+      Position.create(:name => "center mid",:formation_id => formation_id)
+      Position.create(:name => "right mid",:formation_id => formation_id)
+      Position.create(:name => "left defender",:formation_id => formation_id)
+      Position.create(:name => "stopper",:formation_id => formation_id)
+      Position.create(:name => "right defender",:formation_id => formation_id)
+      Position.create(:name => "sweeper",:formation_id => formation_id)
+      Position.create(:name => "goalie",:formation_id => formation_id)
     end
   end
 end
