dd0e48a206ea7fe228c9233fd56beee6901ab17f
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-04 09:21:21 -0700

    Seed application with starter formations and positions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-Position.create(:name => "Sweeper", :type => "defender", :team_id => )
+form1 = Formation.create(:name => "4-4-2")
+form2 = Formation.create(:name => "4-3-3")
 
+Position.create(:name => "sweeper",:formation_id => form1)
+Position.create(:name => "sweeper",:formation_id => form2)
 
- t.string :name
-      t.string :type
-      t.integer :team_id
+Position.create(:name => "stopper",:formation_id => form1)
+Position.create(:name => "stopper",:formation_id => form2)
 
+Position.create(:name => "right defender",:formation_id => form1)
+Position.create(:name => "right defender",:formation_id => form2)
+
+Position.create(:name => "left defender",:formation_id => form1)
+Position.create(:name => "left defender",:formation_id => form2)
+
+Position.create(:name => "left mid",:formation_id => form1)
+Position.create(:name => "left mid",:formation_id => form2)
+
+Position.create(:name => "right mid",:formation_id => form1)
+Position.create(:name => "right mid",:formation_id => form2)
+
+Position.create(:name => "center mid 1",:formation_id => form1)
+Position.create(:name => "center mid",:formation_id => form2)
+
+Position.create(:name => "center mid 2",:formation_id => form1)
+Position.create(:name => "center forward",:formation_id => form2)
+
+Position.create(:name => "left forward",:formation_id => form1)
+Position.create(:name => "left forward",:formation_id => form2)
+
+Position.create(:name => "right forward",:formation_id => form1)
+Position.create(:name => "right forward",:formation_id => form2)
