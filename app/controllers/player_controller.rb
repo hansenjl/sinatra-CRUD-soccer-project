97730ffb179cf36f554a99c0270703e5d15974e1
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-09 18:30:34 -0700

    Fix player validation for player editing and changed finding players to rely on the current_team and not all players

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -29,7 +29,7 @@ class PlayerController < ApplicationController
 
   get "/player/:id" do
     if is_logged_in?
-      @player = Player.find(params[:id])
+      @player = @current_team.players.find(params[:id])
       erb :'player/show'
     else
       redirect "/"
@@ -38,7 +38,7 @@ class PlayerController < ApplicationController
 
   get "/player/:id/edit" do
     if is_logged_in?
-      @player = Player.find(params[:id])
+      @player = @current_team.players.find(params[:id])
       erb :'player/edit'
     else
       redirect "/"
@@ -47,7 +47,7 @@ class PlayerController < ApplicationController
 
   get "/player/:id/delete" do
     if is_logged_in?
-      @player = Player.find(params[:id])
+      @player = @current_team.players.find(params[:id])
       @player.delete
       redirect "/teams/#{@current_team.id}"
     else
@@ -57,17 +57,16 @@ class PlayerController < ApplicationController
 
   patch "/player/:id" do
     if is_logged_in?
-      if !@current_team.players.find_by(:number => params[:number]).nil?
-        flash[:message] = "Unable to add player because the number #{params[:number]} is already taken. Choose a different number and try again."
-        erb :"/player/new"
-      elsif !params[:name].empty? && !params[:number].empty?
-        @player = Player.find(params[:id])
-      @player.update(:name => params[:name], :number => params[:number])
-      @player.save
-      redirect "/player/#{@player.id}"
+      @player = @current_team.players.find(params[:id])
+      if !@current_team.players.find_by(:number => params[:number]).nil? && @player.number != params[:number].to_i
+        binding.pry
+        flash[:message] = "Unable to edit player because the number #{params[:number]} is already taken. Choose a different number and try again."
+        erb :"/player/edit"
       else
-         flash[:message] = "A new player must have both a name and a number"
-        erb :"/player/new"
+        @player.update(:name => params[:name]) if !params[:name].empty?
+        @player.update(:number => params[:number]) if !params[:number].empty?
+        @player.save
+        redirect "/player/#{@player.id}"
       end
     else
       redirect "/"
